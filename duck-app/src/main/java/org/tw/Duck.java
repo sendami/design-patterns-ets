ca4705bbc0d4cc2f4597a0791ebd59deffc6babd
sendami luque <sendami.luque@thoughtworks.com>
2023-04-14 09:21:14 +0200

    Add Model duck and FlyRocketPowered flying behaviour

--- duck-app/src/main/java/org/tw/Duck.java
+++ duck-app/src/main/java/org/tw/Duck.java
@@ -6,10 +6,15 @@ public abstract class Duck {
     public String swim(){
         return "swimming!!";
     }
+    public abstract String display();
     public String performFly(){
         return flyBehaviour.fly();
     }
     public String performQuack(){return quackBehaviour.quack();}
-
-    public abstract String display();
+    public void setFlyBehaviour(FlyBehaviour fb) {
+        this.flyBehaviour = fb;
+    }
+    public void setQuackBehaviour(QuackBehaviour qb) {
+        this.quackBehaviour = qb;
+    }
 }
